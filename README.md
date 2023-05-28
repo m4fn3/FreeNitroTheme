8bf5e5653f31d0938008ea5aac432f7d1fa76e79
mafu <mafusukee@gmail.com>
2023-05-28 20:08:05 +0900

    First

---
+++ README.md
@@ -0,0 +1,4 @@
+# BetterBioText
+
+## Install
+https://raw.githubusercontent.com/m4fn3/BetterBioText/master/dist/BetterBioText.js
---
+++ src/index.tsx
@@ -0,0 +1,73 @@
+import {Plugin, registerPlugin} from 'enmity/managers/plugins'
+import {React} from 'enmity/metro/common'
+import {create} from 'enmity/patcher'
+// @ts-ignore
+import manifest, {name as plugin_name} from '../manifest.json'
+import Settings from "./components/Settings"
+import {getByProps} from "enmity/modules"
+import {get, set} from "enmity/api/settings"
+
+// Make sure to disable "sync across clients"
+
+const Themer = getByProps("updateBackgroundGradientPreset")
+const Stat = getByProps("canUseClientThemes")
+const {ExperimentStore} = getByProps('useAndTrackExposureToUserExperiment')
+
+const Patcher = create('FreeNitroTheme')
+
+const FreeNitroTheme: Plugin = {
+    ...manifest,
+    onStart() {
+        // ※ shouldSync = false で一応オフにできる
+        // const SyncedUserSettingsStore = getModule((m) => m?.getName?.() === "SelectivelySyncedUserSettingsStore");
+        // SyncedUserSettingsStore.getState()
+
+        // apply theme on startup before Discord applies it. we can't wait  Discord to load it
+        if (get(plugin_name, "theme", -1) > 0) {
+            Themer.updateBackgroundGradientPreset(get(plugin_name, "theme", -1))
+        }
+
+        // enable client theme experiment
+        Patcher.after(ExperimentStore, 'getUserExperimentDescriptor', (self, [expName], res) => {
+            if (expName === "2023-02_client_themes_mobile" && res?.bucket) {
+                return {
+                    type: 'user',
+                    revision: 1,
+                    population: 0,
+                    bucket: 1,
+                    override: true
+                }
+            }
+        })
+
+        // make client theme available
+        Patcher.instead(Stat, "canUseClientThemes", (_, args, __) => {
+            return true
+        })
+
+        // detect theme selection
+        Patcher.after(Themer, "updateMobilePendingThemeIndex", (_, args, __) => {
+            // it uses mobileThemesIndex, which is different from presetId
+            if (0 <= args[0] && args[0] <= 2) { // non nitro client theme
+                set(plugin_name, "theme", -1)  // at this time, update background gradient preset won't be called. so I need to reset the value I store manually
+            }
+            // console.log(`change: ${args[0]}`)
+        })
+
+        // detect theme application
+        Patcher.after(Themer, "updateBackgroundGradientPreset", (_, args, __) => {
+            // on change or on apply at startup (uses presetId)
+            // 8~19: dark / 0~7: light
+            set(plugin_name, "theme", args[0])
+            // console.log(`apply: ${args[0]}`)
+        })
+    },
+    onStop() {
+        Patcher.unpatchAll()
+    },
+    getSettingsPanel({settings}) {
+        return <Settings settings={settings}/>
+    }
+}
+
+registerPlugin(FreeNitroTheme)
